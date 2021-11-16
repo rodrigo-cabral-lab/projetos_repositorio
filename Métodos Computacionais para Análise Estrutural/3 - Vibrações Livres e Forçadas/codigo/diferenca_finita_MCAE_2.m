cf90e2af6c183c126eb160e3e7628b834e21d9e1
Rodrigo Cabral <83255807+rodrigo-cabral-lab@users.noreply.github.com>
2021-11-16 12:24:21 -0300

    Update diferenca_finita_MCAE_2.m

--- Métodos Computacionais para Análise Estrutural/3 - Vibrações Livres e Forçadas/codigo/diferenca_finita_MCAE_2.m
+++ Métodos Computacionais para Análise Estrutural/3 - Vibrações Livres e Forçadas/codigo/diferenca_finita_MCAE_2.m
@@ -1,23 +1,22 @@
 %arquivo diferencas_finitas_MCAE_2.m
-%julho 2016 - Prof. Reyolando Brasil
-%integração no tempo por diferenças finitas
-%vários graus de liberdade
+%integraÃ§Ã£o no tempo por diferenÃ§as finitas
+%vÃ¡rios graus de liberdade
 %Exemplo: viga com massas em hastes sobre os apoios
 clear all
 close all
 clc
 %
-nn = 2; % número de graus de liberdade
+nn = 2; % nÃºmero de graus de liberdade
 %
-%dimensões das matrizes
+%dimensÃµes das matrizes
 %
 % entrada das matrizes
-M=[1*0.5^2 0;0 1*0.5^2]; %matriz de massa tm²
+M=[1*0.5^2 0;0 1*0.5^2]; %matriz de massa tmÂ²
 K=[40 20;20 40]; %matriz de rigidez KNm/rad
 %
 tf=25; %tempo final em segundos
-h=0.025;% passo de integração em segundos
-np=tf/h; % número de passos de integração
+h=0.025;% passo de integraÃ§Ã£o em segundos
+np=tf/h; % nÃºmero de passos de integraÃ§Ã£o
 p=zeros(nn,np);%vetor de carregamento
 u=zeros(nn,np);%vetor de deslocamentos
 up=zeros(nn,np);%vetor de velocidades
@@ -28,23 +27,23 @@ dp=zeros(np,1);
 alfa=1;beta= 0.0;% %coef de amortecimento de Rayleigh
 C=alfa*M+beta*K;%matriz de amortecimento de Rayleigh
 %
-%constantes do método
+%constantes do mÃ©todo
 %
 KC=M/h/h+C/2/h;A1=K-2*M/h/h;A2=M/h/h-C/2/h;
 %
-% condições inciais
+% condiÃ§Ãµes inciais
 %u(:,1)= ;up(:,1); % consideradas nulas no exemplo
 t(1)=0;
-p(:,1)=[sin(t(1));sin(t(1))];%função carregamento
+p(:,1)=[sin(t(1));sin(t(1))];%funÃ§Ã£o carregamento
 %
-upp=M\(p(:,1)-C*up(:,1)-K*u(:,1));%aceleração incial
-u(:,2)=upp*h*h/2+h*up(:,1)+u(:,1);%deslocamento no início do segundo passo
+upp=M\(p(:,1)-C*up(:,1)-K*u(:,1));%aceleraÃ§Ã£o incial
+u(:,2)=upp*h*h/2+h*up(:,1)+u(:,1);%deslocamento no inÃ­cio do segundo passo
 %
 %loop principal
 %
 for n=2:np
     t(n)=t(n-1)+h;
-    p(:,n)=[sin(t(n));sin(t(n))]; %chamar função carregamento
+    p(:,n)=[sin(t(n));sin(t(n))]; %chamar funÃ§Ã£o carregamento
     pc=p(:,n)-A1*u(:,n)-A2*u(:,n-1);
     u(:,n+1)=KC\pc;
     up(:,n)=(u(:,n+1)-u(:,n-1))/(2*h);
@@ -52,7 +51,7 @@ end
 %
 %plotagem de deslocamentos
 %
-% direção 1
+% direÃ§Ã£o 1
 %
 for i=1:np
     dp(i,1)=u(1,i);
@@ -60,10 +59,10 @@ end
 figure(1)
 plot(t,dp)
 xlabel('t (s)')
-ylabel('Deslocamentos direção 1 (rad)')
+ylabel('Deslocamentos direÃ§Ã£o 1 (rad)')
 grid
 %
-% direção 2
+% direÃ§Ã£o 2
 %
 for i=1:np
     dp(i,1)=u(2,i);
@@ -71,7 +70,7 @@ end
 figure(2)
 plot(t,dp)
 xlabel('t (s)')
-ylabel('Deslocamentos direção 2 (rad)')
+ylabel('Deslocamentos direÃ§Ã£o 2 (rad)')
 grid
 %
     
